d4a97dea9317b7f33ac11e0813de10ac44eb1ef5
ianyanzhang <ianyanzhang@tencent.com>
2022-12-01 21:37:22 +0800

    1、Bug Fix

--- Demo/TXLiteAVDemo/SuperPlayerKit/SuperPlayer/Localized/SuperPlayerLocalized.m
+++ Demo/TXLiteAVDemo/SuperPlayerKit/SuperPlayer/Localized/SuperPlayerLocalized.m
@@ -8,8 +8,24 @@
 #import "SuperPlayerLocalized.h"
 
 NSString *superPlayerLocalizeFromTable(NSString *key, NSString *table) {
-    NSString *resourcePath = [[NSBundle mainBundle] pathForResource:@"SuperPlayerKitBundle" ofType:@"bundle"];
-    NSBundle *bundle = [NSBundle bundleWithPath:resourcePath];
+    NSArray *languages = [[NSUserDefaults standardUserDefaults] objectForKey:@"AppleLanguages"];
+    if ([languages count] <= 0) {  // 语言包判断
+        return key;
+    }
+    
+    NSString *preferredLang = [languages objectAtIndex:0]; // zh-Hant-GB
+    NSString *languageProj = [preferredLang stringByReplacingOccurrencesOfString:@"-GB" withString:@""];
+    NSString *resourceDict = [[NSBundle mainBundle] pathForResource:@"SuperPlayerKitBundle" ofType:@"bundle"];
+    if (resourceDict.length == 0) { // 资源Bundle文件目录
+        return key;
+    }
+    
+    NSString *bundleOfPath = [NSBundle pathForResource:languageProj ofType:@"lproj" inDirectory:resourceDict];
+    NSBundle *bundle = [NSBundle bundleWithPath:bundleOfPath];
+    if (!bundle) { // 语言lproj的判断
+        return key;
+    }
+    
     return [bundle localizedStringForKey:key value:@"" table:table];
 }
 
