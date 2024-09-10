260b254797ac3edcb7c08a55fe7f9abb6ae5e655
dokieyang <dokieyang@tencent.com>
2024-09-10 14:32:17 +0800

    delete  license

--- Player-API-Example-iOS/PlayerApiDemo/PlayerApiDemo/AppModule/AppDelegate.m
+++ Player-API-Example-iOS/PlayerApiDemo/PlayerApiDemo/AppModule/AppDelegate.m
@@ -37,7 +37,7 @@
     [self.window makeKeyAndVisible];
     
     // 设置licence
-    [TXLiveBase setLicenceURL:@"https://license-test.vod2.myqcloud.com/test/license/testV2/1253968938_1/v_cube.license" key:@"79aa077679db4b0d68643e7af3516719"];
+    [TXLiveBase setLicenceURL:@"your_liense_url" key:@"your_license_key"];
     
     return YES;
 }
